3eaff52eaa71cdf63f87d474f5c356e1e6157327
brotakovici <brotakovici@gmail.com>
2014-07-01 19:01:52 +0300

    Add passport.config

---
+++ config/passport.js
@@ -0,0 +1,48 @@
+var LocalStrategy = require('passport-local').Strategey;
+var User = require('../app/models/user');
+
+module.exports= function (passport) {
+  
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser( function (id, done) {
+    User.findById (id, function (err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use ('local-signup', new LocalStrategy({
+      username : 'email',
+      password : 'password',
+      passReqToCallback : true
+    },
+  
+    function (req, email, password, done) {
+      process.nextTick(function () {
+        User.findOne({ 'local.email' : email}, function (err, user) {
+           
+          if (err) { 
+          return done(err);
+          } 
+          
+          if (user) {
+            return done(null, false, req.flash('signupMessage', 'Email is already taken'));  
+          } else {
+            var newUser = new User();
+            newUser.local.email = email;
+            newUser.local.password = newUser.generateHash(password);
+
+            // Save the user
+            newUser.save(function (err){
+              if (err) {
+                throw err;
+              }
+              return done(null, newUser);
+            });
+          }
+        });
+      });
+    }));
+};

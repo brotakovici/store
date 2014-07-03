82130266014c828a45d3e4b83da131128edbb215
brotakovici <brotakovici@gmail.com>
2014-07-03 11:02:49 +0300

    Add sign-up local strategy

--- config/passport.js
+++ config/passport.js
@@ -43,4 +43,26 @@ module.exports = function (passport) {
         });
       });
     }));
+
+  passport.use('local-login', new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+    }, function(req, email, password, done) {
+         User.findOne({'local.email': email}, function(err, user){
+           if (err) {
+             return done(err);
+           }
+
+           if (!user) {
+             return done(null, false, req.flash('loginMessage', 'No such user found'));
+           }
+
+           if (!user.validPassword(password)) {
+             return done(null, false, req.flash('loginMessage', 'Wrong password'));
+           }
+ 
+           return done(null, user);
+         });
+       }));
 };

c798f0f31691ae4a57f3826f470163b433d30fa2
Andrei Maximilian Diamandopol <brotakovici@gmail.com>
2014-10-10 14:32:57 +0300

    Added edit script

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,6 +1,7 @@
 (function(){
 
  // Inputs!
+ var elemId = document.getElementById('id');
  var elemName = document.getElementById('name');  
  var elemEmail = document.getElementById('email');  
  var elemPhone = document.getElementById('phone');  
@@ -13,5 +14,23 @@
 var elemSaveBtn = document.getElementById('save-button');
 var elemCancelBtn = document.getElementById('cancel-button');
 
+attachEventListener(elemSaveBtn, 'click', function() {
+  
+  var data = {
+    '_id': elemId.value, 
+    'name': elemName.value,
+    'local.email': elemEmail.value,
+    'phone': elemPhone.value,
+    'address': elemAddress,
+    'city': elemCity.value,
+    'county': elemCounty,
+    'postcode': elemPostcode
+  };
+
+  HTTPRequest.put('/edit', data, function(status, header, content){
+    // Error mngmnt goes here!!
+    document.location = '/profile';
+  });
+});
 
 })();
